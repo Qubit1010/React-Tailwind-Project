d0b72486fa4f978262f9986be2043409cb346273
Qubit1010 <hassanaleem86@gmail.com>
2023-01-04 00:41:06 +0500

    Setup Initialized & First section completed

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px"
+    },
+    extend: {
+      colors: {
+        bgTheme: "#141620;",
+        bgThemeTwo: "#000000;",
+        textCol: "#596184;",
+        btnBgCol: "#D5FF64"
+      }
+    }
+  },
+  plugins: [],
+}
