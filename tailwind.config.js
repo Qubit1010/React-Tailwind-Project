4ef15e944300a9baa0279573de769f74d8c53de4
Qubit1010 <hassanaleem86@gmail.com>
2023-01-04 02:17:50 +0500

    Homepage intialized, Header & Footer completed.

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,18 @@ module.exports = {
         bgThemeTwo: "#000000;",
         textCol: "#596184;",
         btnBgCol: "#D5FF64",
-        popUpBgCol: "#B7006E"
+        popUpBgCol: "#B7006E",
+        borderDividerCol: "#e5e7eb17",
       }
+    },
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '.5px',
+     '3': '1px',
+      '4': '2px',
+     '6': '4px',
+     '8': '6px',
     }
   },
   plugins: [],
